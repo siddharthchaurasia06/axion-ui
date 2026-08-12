137ef7f7be6556a23158c2ea256849d6a2eb7ad6
siddharthchaurasia06 <88610301+siddharthchaurasia06@users.noreply.github.com>
2026-08-12 16:14:25 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,8 @@ import { SystemSettings } from './components/pages/SystemSettings';
 import { DashboardView } from './components/pages/DashboardView';
 import { NotFound } from './components/pages/NotFound';
 
-const API_BASE = 'https://api.axionsystems.de';
+// const API_BASE = 'https://api.axionsystems.de';
+const API_BASE = 'http://10.0.31.15:8000/';
 
 function App() {
   const location = useLocation();
